f6a0d7c8816a429e2ea4920f5291a6cbcf3f3f5a
Natalya Babenko <natapervomaisk88@gmail.com>
2023-04-05 17:34:35 +0300

    added two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,9 +8,20 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CarComponent } from './components/car/car.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { MycustompipePipe } from './pipes/mycustompipe.pipe';
+import { ImageComponent } from './components/image/image.component';
+import { FormsModule } from '@angular/forms';
+import { InputCarComponent } from './components/input-car/input-car.component';
 
 @NgModule({
-  declarations: [AppComponent, CarComponent, CounterComponent],
+  declarations: [
+    AppComponent,
+    CarComponent,
+    CounterComponent,
+    MycustompipePipe,
+    ImageComponent,
+    InputCarComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -19,6 +29,7 @@ import { CounterComponent } from './components/counter/counter.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,17 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { ICar } from 'src/app/models/ICar';
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.scss'],
 })
 export class CarComponent {
-  private title: string = 'List of cars';
-  private cars = ['audi', 'opel', 'mazda'];
-  public get Title() {
-    return this.title;
-  }
-  public get Cars() {
-    return this.cars;
-  }
+  @Input('item') car: ICar;
 }
